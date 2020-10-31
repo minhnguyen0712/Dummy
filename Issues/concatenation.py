0217e75623cb4fc80dc991bbea8b10e4e941cabc
minh.ngh <ngh.nhatminh0712@gmail.com>
2020-10-31 19:26:55 +0700

    :tada: Added concatenation problems

---
+++ Issues/concatenation.py
@@ -0,0 +1,15 @@
+# Concatenation problem
+# Input: two strings
+# Output: concat of two strings
+
+
+def concat(s1, s2):
+    return str(s1) + str(s2)
+
+
+def concat1(string_list):
+    return "".join(string_list)
+
+
+print(concat("a", "b"))
+print(concat1(["a", "b", "c"]))
