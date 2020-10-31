01dac68f5380fa38b5c7e075c303cd1297453576
minh.ngh <ngh.nhatminh0712@gmail.com>
2020-10-31 17:43:53 +0700

    :tada: Added backtracking problems in Python

---
+++ Issues/backtracking.py
@@ -0,0 +1,15 @@
+# Backtracking problems
+# Input: order arrangements of possibilities
+# Output: list contains of all possibilities
+
+
+def backtrack(order, letters_list):
+    if(order == 1):
+        return letters_list
+    else:
+        return [y + x for y in backtrack(1, letters_list) for x in backtrack(order - 1, letters_list)]
+
+
+print(backtrack(1, ["a", "b", "c"]))
+print(backtrack(2, ["a", "b", "c"]))
+print(backtrack(3, ["a", "b", "c"]))
